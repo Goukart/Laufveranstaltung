6ab1db30ee9112acd11be17f3148977751c8de3e
Bonjamin <benjamin.herzberger@hs-furtwangen.de>
2018-04-19 11:29:20 +0200

    Logger Library eingebunden

--- src/Veranstaltung.java
+++ src/Veranstaltung.java
@@ -1,5 +1,5 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import java.util.Date;
 import java.util.Map;
 
@@ -14,7 +14,7 @@ public class Veranstaltung implements Bank {
 	
 	
 	public static void main(String[] argv) {
-		
+		System.out.println("Passt erstmal");
 	}
 	
 	
